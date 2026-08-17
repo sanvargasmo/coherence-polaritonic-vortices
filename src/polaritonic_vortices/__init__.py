b6a299bb54a0b7245d41116ac9aec758845dad8a
sanvargasmo <110981293+sanvargasmo@users.noreply.github.com>
2026-08-16 22:54:21 -0500

    Export spatial reconstruction utilities

--- src/polaritonic_vortices/__init__.py
+++ src/polaritonic_vortices/__init__.py
@@ -11,6 +11,19 @@ from .reduced_density import (
     purity,
 )
 from .observables import cavity_lz, exciton_lz, total_lz
+from .spatial_fields import (
+    cavity_spatial_density,
+    exciton_spatial_density,
+    joint_wavefunction_same_coordinate,
+    cavity_transition_field_x,
+    cavity_transition_field_y,
+    exciton_transition_field_x,
+    exciton_transition_field_y,
+    cavity_spatial_density_grid,
+    exciton_spatial_density_grid,
+    joint_wavefunction_grid_same_coordinate,
+    transition_field_grid,
+)
 
 __all__ = [
     "PhysicalParameters",
@@ -30,4 +43,15 @@ __all__ = [
     "cavity_lz",
     "exciton_lz",
     "total_lz",
+    "cavity_spatial_density",
+    "exciton_spatial_density",
+    "joint_wavefunction_same_coordinate",
+    "cavity_transition_field_x",
+    "cavity_transition_field_y",
+    "exciton_transition_field_x",
+    "exciton_transition_field_y",
+    "cavity_spatial_density_grid",
+    "exciton_spatial_density_grid",
+    "joint_wavefunction_grid_same_coordinate",
+    "transition_field_grid",
 ]
